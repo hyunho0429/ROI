997808ff6daa13c3f85661d07003d5a271ee61d4
hyunho0429 <hunho79248@gmail.com>
2026-07-25 17:11:57 +0900

    Further soften Stanley steering

--- src/path_planning/src/path_planning/pure_pursuit_udp_runtime.py
+++ src/path_planning/src/path_planning/pure_pursuit_udp_runtime.py
@@ -161,16 +161,16 @@ def argument_parser(localization_mode):
     parser.add_argument("--lookahead-speed-gain", type=float, default=0.15)
     parser.add_argument("--minimum-lookahead", type=float, default=2.0)
     parser.add_argument("--maximum-lookahead", type=float, default=4.0)
-    parser.add_argument("--stanley-gain", type=float, default=0.38)
-    parser.add_argument("--softening-speed", type=float, default=2.0)
-    parser.add_argument("--heading-error-gain", type=float, default=0.85)
-    parser.add_argument("--cross-track-error-gain", type=float, default=0.60)
+    parser.add_argument("--stanley-gain", type=float, default=0.35)
+    parser.add_argument("--softening-speed", type=float, default=2.2)
+    parser.add_argument("--heading-error-gain", type=float, default=0.78)
+    parser.add_argument("--cross-track-error-gain", type=float, default=0.55)
     parser.add_argument("--cross-track-deadband", type=float, default=0.05)
     parser.add_argument("--goal-tolerance", type=float, default=2.0)
     parser.add_argument(
         "--max-steering-deg",
         type=float,
-        default=18.0,
+        default=16.5,
         help="Stanley controller steering limit in degrees",
     )
     parser.add_argument(
